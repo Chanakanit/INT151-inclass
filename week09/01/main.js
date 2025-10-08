00cecde4c7e167e1075805211c05238053a6f811
Chanakanit <177916223+Chanakanit@users.noreply.github.com>
2025-10-08 18:49:05 +0700

    week 09

--- week09/01/main.js
+++ week09/01/main.js
@@ -1,15 +1,27 @@
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const confirmPass = document.getElementById('confirm-password')
-const form = document.querySelector('form')
-const p = document.querySelector('p')
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPass = document.getElementById("confirm-password");
+const form = document.querySelector("form");
+const p = document.querySelector("p");
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (password.value !== confirmPass.value) {
+    p.style.color = "red";
+    p.textContent = "password and comfirm do not match, check again";
+  } else if (
+    !username.value ||
+    !email.value ||
+    !password.value ||
+    !confirmPass.value
+  ) {
+    p.style.color = "red";
+    p.textContent = "missing some values, please try again!";
+  } else {
+    p.style.color = "green";
+    p.textContent = 'your data completed'
+  }
 
-form.addEventListener('submit', () => {
-    if(!username.value || !email.value || !password.value || !confirmPass.value) {
-        console.log('')
-    }
-})
-
-
+    
+});
